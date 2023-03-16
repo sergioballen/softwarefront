5b306aaa5f8c03d6a6d56e7776bce5548bab20af
Julieth-Plazas <juliethpr9@gmail.com>
2023-03-16 11:04:21 -0500

    Inicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './Components/login/login.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { BarraLateralComponent } from './Components/barra-lateral/barra-lateral.component';
+import { HomeComponent } from './Components/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    BarraLateralComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    LayoutModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
